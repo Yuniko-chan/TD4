71eb9427d692c6107fafb3b4fd493e7a9d3d391c
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2025-06-05 01:07:25 +0900

    カスタムエリア追加

--- Project/Application/Object/Car/PickupPoint/User/ArmorPickupPoint.cpp
+++ Project/Application/Object/Car/PickupPoint/User/ArmorPickupPoint.cpp
@@ -4,6 +4,6 @@ void ArmorPickupPoint::Initialize(LevelData::MeshData* data)
 {
 	// 初期化
 	IPickupPoint::Initialize(data);
-	worldTransform_.transform_.translate = { 20.0f,0.0f,0.0f };
+	//worldTransform_.transform_.translate = { 20.0f,0.0f,0.0f };
 	type_ = kArmor;
 }
--- Project/Application/Object/Car/PickupPoint/User/EnginePickupPoint.cpp
+++ Project/Application/Object/Car/PickupPoint/User/EnginePickupPoint.cpp
@@ -4,7 +4,7 @@ void EnginePickupPoint::Initialize(LevelData::MeshData* data)
 {
 	// 初期化
 	IPickupPoint::Initialize(data);
-	worldTransform_.transform_.translate = { 0.0f,0.0f,20.0f };
+	//worldTransform_.transform_.translate = { 0.0f,0.0f,20.0f };
 
 	type_ = kEngine;
 }
