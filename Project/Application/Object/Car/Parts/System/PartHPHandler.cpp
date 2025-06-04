cade28dcce013579cee70c37c1bbb206d1d84bda
nomu <@MSI>
2025-06-04 10:34:43 +0900

    パーツのHP管理クラスに無敵時間処理追加

--- Project/Application/Object/Car/Parts/System/PartHPHandler.cpp
+++ Project/Application/Object/Car/Parts/System/PartHPHandler.cpp
@@ -1,5 +1,6 @@
 #include "PartHPHandler.h"
 #include "../PartsInterface.h"
+#include "../../../../../Engine/Math/DeltaTime.h"
 
 void PartHPHandler::Initialize()
 {
@@ -15,13 +16,45 @@ void PartHPHandler::Update()
 		// 解除フラグ
 		owner_->SetIsDelete(true);
 	}
+
+	// 無敵経過
+	InvisibleProgress();
+
 }
 
 void PartHPHandler::OnHit(int16_t damage)
 {
+
+	// 無敵状態なのでリターン
+	if (isInvisible_) {
+		return;
+	}
+	else {
+		isInvisible_ = true;
+		invisibleCooltime_ = 0.0f;
+	}
+
 	hp_ -= damage;
 	// 0より小さい値にならないように
 	if (hp_ < 0) {
 		hp_ = 0;
 	}
+
+}
+
+void PartHPHandler::InvisibleProgress()
+{
+
+	// 無敵か
+	if (isInvisible_) {
+		// 経過
+		invisibleCooltime_ += kDeltaTime_;
+		// 無敵最大時間
+		const float kInvisibleTimeMax = 1.0f;
+		// 無敵終了か
+		if (invisibleCooltime_ >= kInvisibleTimeMax) {
+			isInvisible_ = false;
+		}
+	}
+
 }
