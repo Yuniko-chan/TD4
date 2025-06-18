5350f8513e1ffe0b2bf13dc428aacbe4f40a3156
Hnaoyan <k022g0041@g.neec.ac.jp>
2025-06-18 11:41:59 +0900

    オーバーヒートの対象をひとつに（乱数）

--- Project/Application/Object/Car/System/Damage/OverheatSystem.cpp
+++ Project/Application/Object/Car/System/Damage/OverheatSystem.cpp
@@ -1,5 +1,6 @@
 #include "OverheatSystem.h"
 #include "../../VehicleCore.h"
+#include "../../../Engine/Math/RandomEngine.h"
 
 void OverheatSystem::Update()
 {
@@ -8,6 +9,16 @@ void OverheatSystem::Update()
 		return;
 	}
 
+	// 深度情報更新
+	preDepth_ = maxDepth_;
+
+	// 最大深度値検索
+	for (std::map<Vector2Int, Car::IParts*>::iterator it = this->mappingData_->begin();
+		it != mappingData_->end(); ++it) {
+		if (maxDepth_ < (*it).first.GetLength()) {
+			maxDepth_ = (*it).first.GetLength();
+		}
+	}
 	// パーツにダメージを与えるメソッド
 	DamageProcess();
 
@@ -15,29 +26,36 @@ void OverheatSystem::Update()
 
 void OverheatSystem::DamageProcess()
 {
-	//Car::IParts* targetPart = nullptr;
-	std::vector<Car::IParts*> depths = {};
-	int maxDepth = 0;
-	// 最大深度値検索
-	for (std::map<Vector2Int, Car::IParts*>::iterator it = this->mappingData_->begin();
-		it != mappingData_->end(); ++it) {
-		if (maxDepth < (*it).first.GetLength()) {
-			maxDepth = (*it).first.GetLength();
-
-		}
-
+	// 前フレームの深度と最大深度が異なれば対象を変更する処理
+	if (preDepth_ != maxDepth_) {
+		AssignRandomOverheatPart();
+	}
+	// 対象パーツが外れて親が亡くなった時に対象を更新
+	else if (!targetPart_->IsParent()) {
+		AssignRandomOverheatPart();
 	}
 
-	// 対象をリスト化
+	// 対象にダメージ
+	if (targetPart_) {
+		targetPart_->GetHPHandler()->HeatDamage();
+	}
+}
+
+void OverheatSystem::AssignRandomOverheatPart()
+{
+	// 対象
+	std::vector<Car::IParts*> depths = {};
+	// 最大深度
+	int maxDepth = maxDepth_;
+	// 最大深度の物をリスト化
 	for (std::map<Vector2Int, Car::IParts*>::iterator it = this->mappingData_->begin();
 		it != mappingData_->end(); ++it) {
 		if (maxDepth == (*it).first.GetLength()) {
 			depths.push_back((*it).second);
 		}
 	}
-
-	for (std::vector<Car::IParts*>::iterator it = depths.begin(); it != depths.end(); ++it) {
-		(*it)->GetHPHandler()->HeatDamage();
-	}
-
+	// 番号をランダムで決定
+	int targetIndex = (int)RandomEngine::GetRandom(0, (float)depths.size() - 1);
+	// 番号のパーツを設定
+	targetPart_ = depths[targetIndex];
 }
--- Project/Application/Object/Car/System/Damage/OverheatSystem.h
+++ Project/Application/Object/Car/System/Damage/OverheatSystem.h
@@ -17,8 +17,17 @@ private:
 	/// </summary>
 	void DamageProcess();
 
+	/// <summary>
+	/// オーバーヒートの対象を割り当てる
+	/// </summary>
+	void AssignRandomOverheatPart();
+
 private:
 	// パーツのマッピングデータ
 	std::map<Vector2Int, Car::IParts*>* mappingData_ = nullptr;
-
+	// 対象のパーツ
+	Car::IParts* targetPart_ = nullptr;
+	// 前フレームの深度情報（ここが更新されなければダメージ対象を変えない
+	int preDepth_ = 0;
+	int maxDepth_ = 0;
 };
