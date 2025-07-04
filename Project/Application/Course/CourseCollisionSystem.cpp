5899a9d5798ef44724a8ae960c5d882d09e9c1b5
nomu <@MSI>
2025-07-04 14:53:10 +0900

    コース衝突クラスに、ギミックを設定できるように

--- Project/Application/Course/CourseCollisionSystem.cpp
+++ Project/Application/Course/CourseCollisionSystem.cpp
@@ -279,6 +279,151 @@ void CourseCollisionSystem::SetCustomArea(CustomArea* customArea)
 
 }
 
+void CourseCollisionSystem::SetGimmick(OBB* obb)
+{
+
+	// ポリゴン
+	CoursePolygon polygon = {};
+	polygon.normal = { 0.0f,0.0f,1.0f };
+	polygon.texcoord = { 1.0f, 1.0f };
+	
+	// OBB平面作成
+	Vector3 planeYZ = Vector3::Normalize(Vector3{ 1.0f - std::fabs(obb->otientatuons_[0].x), 1.0f - std::fabs(obb->otientatuons_[0].y), 1.0f - std::fabs(obb->otientatuons_[0].z) });
+	Vector3 planeXZ = Vector3::Normalize(Vector3{ 1.0f - std::fabs(obb->otientatuons_[1].x), 1.0f - std::fabs(obb->otientatuons_[1].y), 1.0f - std::fabs(obb->otientatuons_[1].z) });
+	Vector3 planeXY = Vector3::Normalize(Vector3{ 1.0f - std::fabs(obb->otientatuons_[2].x), 1.0f - std::fabs(obb->otientatuons_[2].y), 1.0f - std::fabs(obb->otientatuons_[2].z) });
+
+	Vector3 size = obb->size_;
+	Vector3 center = obb->center_;
+
+	// 頂点求める
+	Vector3 vertices[8];
+
+	// 左 上 前
+	vertices[0] = {
+		(-size.x * planeYZ.x + size.y * planeXZ.x + -size.z * planeXY.x),
+		(-size.x * planeYZ.y + size.y * planeXZ.y + -size.z * planeXY.y),
+		(-size.x * planeYZ.z + size.y * planeXZ.z + -size.z * planeXY.z)
+	};
+
+	// 左 上 後
+	vertices[1] = {
+		(-size.x * planeYZ.x + size.y * planeXZ.x + size.z * planeXY.x),
+		(-size.x * planeYZ.y + size.y * planeXZ.y + size.z * planeXY.y),
+		(-size.x * planeYZ.z + size.y * planeXZ.z + size.z * planeXY.z)
+	};
+
+	// 右 上 前
+	vertices[2] = {
+		(size.x * planeYZ.x + size.y * planeXZ.x + -size.z * planeXY.x),
+		(size.x * planeYZ.y + size.y * planeXZ.y + -size.z * planeXY.y),
+		(size.x * planeYZ.z + size.y * planeXZ.z + -size.z * planeXY.z)
+	};
+
+	// 右 上 後
+	vertices[3] = {
+		(size.x * planeYZ.x + size.y * planeXZ.x + size.z * planeXY.x),
+		(size.x * planeYZ.y + size.y * planeXZ.y + size.z * planeXY.y),
+		(size.x * planeYZ.z + size.y * planeXZ.z + size.z * planeXY.z)
+	};
+
+	// 左 下 前
+	vertices[4] = {
+		(-size.x * planeYZ.x + -size.y * planeXZ.x + -size.z * planeXY.x),
+		(-size.x * planeYZ.y + -size.y * planeXZ.y + -size.z * planeXY.y),
+		(-size.x * planeYZ.z + -size.y * planeXZ.z + -size.z * planeXY.z)
+	};
+
+	// 左 下 後
+	vertices[5] = {
+		(-size.x * planeYZ.x + -size.y * planeXZ.x + size.z * planeXY.x),
+		(-size.x * planeYZ.y + -size.y * planeXZ.y + size.z * planeXY.y),
+		(-size.x * planeYZ.z + -size.y * planeXZ.z + size.z * planeXY.z)
+	};
+
+	// 右 下 前
+	vertices[6] = {
+		(size.x * planeYZ.x + -size.y * planeXZ.x + -size.z * planeXY.x),
+		(size.x * planeYZ.y + -size.y * planeXZ.y + -size.z * planeXY.y),
+		(size.x * planeYZ.z + -size.y * planeXZ.z + -size.z * planeXY.z)
+	};
+
+	// 右 下 後
+	vertices[7] = {
+		(size.x * planeYZ.x + -size.y * planeXZ.x + size.z * planeXY.x),
+		(size.x * planeYZ.y + -size.y * planeXZ.y + size.z * planeXY.y),
+		(size.x * planeYZ.z + -size.y * planeXZ.z + size.z * planeXY.z)
+	};
+
+	for (size_t i = 0; i < 8; ++i) {
+		vertices[i] += center;
+	}
+
+	Vector3 planeVertices[6][4];
+
+	// 前
+	planeVertices[0][0] = vertices[4];
+	planeVertices[0][1] = vertices[0];
+	planeVertices[0][2] = vertices[6];
+	planeVertices[0][3] = vertices[2];
+
+	// 後
+	planeVertices[1][0] = vertices[7];
+	planeVertices[1][1] = vertices[3];
+	planeVertices[1][2] = vertices[5];
+	planeVertices[1][3] = vertices[1];
+
+	// 右
+	planeVertices[2][0] = vertices[6];
+	planeVertices[2][1] = vertices[2];
+	planeVertices[2][2] = vertices[7];
+	planeVertices[2][3] = vertices[3];
+
+	// 左
+	planeVertices[3][0] = vertices[5];
+	planeVertices[3][1] = vertices[1];
+	planeVertices[3][2] = vertices[4];
+	planeVertices[3][3] = vertices[0];
+
+	// 上
+	planeVertices[4][0] = vertices[0];
+	planeVertices[4][1] = vertices[1];
+	planeVertices[4][2] = vertices[3];
+	planeVertices[4][3] = vertices[2];
+
+	// 下
+	planeVertices[5][0] = vertices[5];
+	planeVertices[5][1] = vertices[4];
+	planeVertices[5][2] = vertices[6];
+	planeVertices[5][3] = vertices[7];
+
+	for (uint32_t i = 0; i < 6; ++i) {
+	
+		// 法線
+		Vector3 v01 = planeVertices[i][1] - planeVertices[i][0];
+		Vector3 v12 = planeVertices[i][2] - planeVertices[i][1];
+		Vector3 v20 = planeVertices[i][0] - planeVertices[i][2];
+
+		// 法線
+		polygon.normal = Vector3::Normalize(Vector3::Cross(v01, v12));
+	
+		polygon.position0 = planeVertices[i][0];
+		polygon.position1 = planeVertices[i][1];
+		polygon.position2 = planeVertices[i][3];
+		
+		// 登録
+		polygons_.push_back(polygon);
+
+		polygon.position0 = planeVertices[i][0];
+		polygon.position1 = planeVertices[i][3];
+		polygon.position2 = planeVertices[i][2];
+
+		// 登録
+		polygons_.push_back(polygon);
+
+	}
+
+}
+
 void CourseCollisionSystem::ClearCorse()
 {
 
