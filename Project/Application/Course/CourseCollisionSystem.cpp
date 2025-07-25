611a6ae76ca727164768f9b702391222cdb89e62
nomu <@MSI>
2025-07-25 12:31:20 +0900

    コース衝突クラスにカスタムエリアを登録しないように

--- Project/Application/Course/CourseCollisionSystem.cpp
+++ Project/Application/Course/CourseCollisionSystem.cpp
@@ -269,42 +269,42 @@ void CourseCollisionSystem::SetCourse(Course* course)
 
 void CourseCollisionSystem::SetCustomArea(CustomArea* customArea)
 {
+	customArea;
+	//// ポリゴン
+	//CoursePolygon polygon = {};
+	//polygon.normal = { 0.0f,1.0f,0.0f };
+	//polygon.texcoord = { 0.0f,0.0f };
 
-	// ポリゴン
-	CoursePolygon polygon = {};
-	polygon.normal = { 0.0f,0.0f,1.0f };
-	polygon.texcoord = { 0.0f,0.0f };
+	//// 高さ
+	//const float kHeight = 0.25f;
+	//// 幅
+	//const float kWidth = 20.0f;
+	//
+	//polygon.position0 = { -kWidth, kHeight, -kWidth };
+	//polygon.position1 = { -kWidth, kHeight, kWidth };
+	//polygon.position2 = { kWidth, kHeight, kWidth };
 
-	// 高さ
-	const float kHeight = 0.25f;
-	// 幅
-	const float kWidth = 20.0f;
-	
-	polygon.position0 = { -kWidth, kHeight, -kWidth };
-	polygon.position1 = { -kWidth, kHeight, kWidth };
-	polygon.position2 = { kWidth, kHeight, kWidth };
+	//// コース中心のワールド座標をプラス
+	//polygon.position0 = Matrix4x4::Transform(polygon.position0, customArea->GetWorldTransformAdress()->worldMatrix_);
+	//polygon.position1 = Matrix4x4::Transform(polygon.position1, customArea->GetWorldTransformAdress()->worldMatrix_);
+	//polygon.position2 = Matrix4x4::Transform(polygon.position2, customArea->GetWorldTransformAdress()->worldMatrix_);
 
-	// コース中心のワールド座標をプラス
-	polygon.position0 = Matrix4x4::Transform(polygon.position0, customArea->GetWorldTransformAdress()->worldMatrix_);
-	polygon.position1 = Matrix4x4::Transform(polygon.position1, customArea->GetWorldTransformAdress()->worldMatrix_);
-	polygon.position2 = Matrix4x4::Transform(polygon.position2, customArea->GetWorldTransformAdress()->worldMatrix_);
+	//// 登録
+	//polygons_[polygonRegistrationNumber_] = polygon;
+	//polygonRegistrationNumber_ = (polygonRegistrationNumber_ + 1) % kCollisionPolygonMax_;
 
-	// 登録
-	polygons_[polygonRegistrationNumber_] = polygon;
-	polygonRegistrationNumber_ = (polygonRegistrationNumber_ + 1) % kCollisionPolygonMax_;
+	//polygon.position0 = { -kWidth, kHeight, -kWidth };
+	//polygon.position1 = { kWidth, kHeight, kWidth };
+	//polygon.position2 = { kWidth, kHeight, -kWidth };
 
-	polygon.position0 = { -kWidth, kHeight, -kWidth };
-	polygon.position1 = { kWidth, kHeight, kWidth };
-	polygon.position2 = { kWidth, kHeight, -kWidth };
+	//// コース中心のワールド座標をプラス
+	//polygon.position0 = Matrix4x4::Transform(polygon.position0, customArea->GetWorldTransformAdress()->worldMatrix_);
+	//polygon.position1 = Matrix4x4::Transform(polygon.position1, customArea->GetWorldTransformAdress()->worldMatrix_);
+	//polygon.position2 = Matrix4x4::Transform(polygon.position2, customArea->GetWorldTransformAdress()->worldMatrix_);
 
-	// コース中心のワールド座標をプラス
-	polygon.position0 = Matrix4x4::Transform(polygon.position0, customArea->GetWorldTransformAdress()->worldMatrix_);
-	polygon.position1 = Matrix4x4::Transform(polygon.position1, customArea->GetWorldTransformAdress()->worldMatrix_);
-	polygon.position2 = Matrix4x4::Transform(polygon.position2, customArea->GetWorldTransformAdress()->worldMatrix_);
-
-	// 登録
-	polygons_[polygonRegistrationNumber_] = polygon;
-	polygonRegistrationNumber_ = (polygonRegistrationNumber_ + 1) % kCollisionPolygonMax_;
+	//// 登録
+	//polygons_[polygonRegistrationNumber_] = polygon;
+	//polygonRegistrationNumber_ = (polygonRegistrationNumber_ + 1) % kCollisionPolygonMax_;
 
 }
 
