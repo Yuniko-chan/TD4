5bdb93e83871699b152a2b559344e5f43a205e9b
nomu <@MSI>
2025-07-28 16:48:13 +0900

    ゲームシーン用のオーディオマネージャー準備

---
+++ Project/Application/AudioManager/GameAudioManager.cpp
@@ -0,0 +1,58 @@
+#include "GameAudioManager.h"
+
+GameAudioManager::~GameAudioManager()
+{
+}
+
+void GameAudioManager::Initialize()
+{
+
+	AudioManager::Initialize();
+
+	// 全体の初期設定
+	for (uint32_t i = 0; i < GameAudioNameIndex::kGameAudioNameIndexOfCount; ++i) {
+		audioDatas_[i].handle_ = audio_->LoadAudio(audioNames_[i]);
+		audioDatas_[i].isLoop_ = false;
+		audioDatas_[i].volume_ = 1.0f;
+		audioDatas_[i].isBGM_ = false;
+	}
+
+	// 個別設定
+
+}
+
+void GameAudioManager::Update()
+{
+}
+
+void GameAudioManager::PlayWave(uint32_t audioIndex)
+{
+
+	float volume = 0.0f;
+	if (audioDatas_[audioIndex].isBGM_) {
+		volume = audioDatas_[audioIndex].volume_ * masterVolume_ * BGMVolume_;
+	}
+	else {
+		volume = audioDatas_[audioIndex].volume_ * masterVolume_ * SEVolume_;
+	}
+
+	IXAudio2SourceVoice* pSourceVoice = audio_->PlayWave(audioDatas_[audioIndex].handle_, audioDatas_[audioIndex].isLoop_, volume);
+	assert(pSourceVoice);
+	for (uint32_t i = 0; i < kMaxPlayingSoundData; ++i) {
+		if (playingSoundDatas_[i].handle_ == audioDatas_[audioIndex].handle_) {
+			if (playingSoundDatas_[i].pSourceVoice_ != nullptr) {
+				playingSoundDatas_[i].pSourceVoice_->DestroyVoice();
+			}
+			playingSoundDatas_[i].pSourceVoice_ = pSourceVoice;
+			playingSoundDatas_[i].volume_ = volume;
+			return;
+		}
+		if (playingSoundDatas_[i].handle_ == audio_->kMaxSoundData) {
+			playingSoundDatas_[i].handle_ = audioDatas_[audioIndex].handle_;
+			playingSoundDatas_[i].pSourceVoice_ = pSourceVoice;
+			playingSoundDatas_[i].volume_ = volume;
+			return;
+		}
+	}
+
+}
---
+++ Project/Application/AudioManager/GameAudioManager.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "../../Engine/Audio/AudioManager.h"
+
+/// <summary>
+/// 名前群
+/// </summary>
+enum GameAudioNameIndex {
+	kGameBGM, // BGM
+	kGameAudioNameIndexOfCount // 数
+};
+
+/// <summary>
+/// ゲームオーディオマネージャー
+/// </summary>
+class GameAudioManager :
+    public AudioManager
+{
+
+public: // メンバ関数
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameAudioManager();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize() override;
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update() override;
+
+	/// <summary>
+	/// 音声再生
+	/// </summary>
+	/// <param name="audioIndex">オーディオの名前</param>
+	/// <returns>再生中のサウンドデータの番号</returns>
+	void PlayWave(uint32_t audioIndex) override;
+
+private: // メンバ変数
+
+	// データ
+	std::array<AudioData, GameAudioNameIndex::kGameAudioNameIndexOfCount> audioDatas_;
+
+	// ファイル名
+	std::array<const std::string, GameAudioNameIndex::kGameAudioNameIndexOfCount> audioNames_ =
+	{
+		"Sound/BGM/BGM.mp3",
+	};
+
+};
+
