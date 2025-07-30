4b4cdbd8e9c713d6ade26aff9a8c27b5be0822df
nomu <@MSI>
2025-07-30 12:09:39 +0900

    タイトルシーンに機能追加

--- Project/Application/TitleSceneObject/TitleSpriteObjects.cpp
+++ Project/Application/TitleSceneObject/TitleSpriteObjects.cpp
@@ -5,14 +5,20 @@ void TitleSpriteObjects::Initialize()
 
 	// 白
 	const Vector4 kWhite = { 1.0f, 1.0f, 1.0f, 1.0f };
-	// ルーレット番号
+	// 透明度変更番号
 	const uint32_t kTransparencyChangeLoopNum = static_cast<uint32_t>(OutGameSpriteObjectAnimation::AnimationIndex::kAnimationIndexTransparencyChangeLoop);
-
+	// 拡張番号
+	const uint32_t kScalingLoopNum = static_cast<uint32_t>(OutGameSpriteObjectAnimation::AnimationIndex::kAnimationIndexScalingLoop);
 
 	// タイトルロゴ
 	titleLogo_ = std::make_unique<OutGameSpriteObject>();
 	titleLogo_->Initialize("Resources/Sprite/Result/traversalNumString.png",
 		Vector2{ 640.0f, 200.0f }, kWhite);
+
+	titleLogo_->GetAnimation()->doesAnimations_[kScalingLoopNum] = true;
+	titleLogo_->GetAnimation()->scalingVariable_.easeName = Ease::EaseName::Lerp;
+	titleLogo_->GetAnimation()->scalingVariable_.start = titleLogo_->GetSize();
+	titleLogo_->GetAnimation()->scalingVariable_.end = titleLogo_->GetSize() + Vector2{ 50.0f,25.0f };
 
 	// ボタンをおしてください
 	pleasePressTheButton_ = std::make_unique<OutGameSpriteObject>();
@@ -20,7 +26,6 @@ void TitleSpriteObjects::Initialize()
 		Vector2{ 640.0f, 600.0f }, kWhite);
 
 	pleasePressTheButton_->GetAnimation()->doesAnimations_[kTransparencyChangeLoopNum] = true;
-
 
 }
 
